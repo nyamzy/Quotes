c1ceb10ff47d5f62197852b199bc2cad149a3def
nyamzy <john.nyamweya@student.moringaschool.com>
2021-07-17 14:14:48 +0300

    transfer all logic from AppComponent to QuoteComponent

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from "../quote";
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes:Quote[] = [
+    {id: 1, name:'The purpose of our lives is to be happy'},
+    {id: 2, name:'Get busy living or get busy dying'},
+  ];
 
   constructor() { }
 
