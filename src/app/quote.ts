bc1f3295a69e8ec51f664868746a1a5c3d1861bc
nyamzy <john.nyamweya@student.moringaschool.com>
2021-07-19 11:28:45 +0300

    add submitter section to form

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   showDescription:boolean;
-  constructor(public id: number, public name: string, public author: string){
+  constructor(public id: number, public name: string, public author: string, public submitter: string){
     this.showDescription = false;
   }
 }
